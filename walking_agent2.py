56e5215485002358d9445aa8ccb7c4e811bde54a
Lavi Avigad <92415264+lavigad1@users.noreply.github.com>
2022-12-28 18:02:12 -0800

    Update walking_agent2.py
    might now work; need to test later

--- walking_agent2.py
+++ walking_agent2.py
@@ -1,4 +1,78 @@
-import gym
-env = gym.make("BipedalWalker-v3", hardcore=True, render_mode='rgb_array')
-env.reset()
-env.render()
+import numpy as np
+
+# Define the environment for the bipedal walker.
+# This could include physical laws, reward/punishment functions, etc.
+def environment(state, action):
+    # Implement the physical laws that govern the movement of the walker, such as gravity and friction.
+    new_state = state + action
+    
+    # Implement a reward/punishment function based on the new state of the walker.
+    # For example, you could give a reward for taking a step forward and a punishment for falling over.
+    reward = 0
+    if new_state[0] > 0:
+        reward = 1
+    elif new_state[0] < 0:
+        reward = -1
+    
+    return new_state, reward
+
+# Define the state space for the bipedal walker.
+# This could be a set of joint angles, positions, velocities, etc.
+# For simplicity, we will define the state space as a single position coordinate.
+def state_space():
+    return np.random.uniform(-1, 1)
+
+# Define the action space for the bipedal walker.
+# This could be a set of joint torques, forces, etc.
+# For simplicity, we will define the action space as a single force applied to the walker.
+def action_space():
+    return np.random.uniform(-1, 1)
+
+# Initialize the Q-table for the bipedal walker.
+# This could be a 2D array with shape (num_states, num_actions).
+# For simplicity, we will define the Q-table as a single value for each state/action pair.
+def initialize_q_table(num_states, num_actions):
+    return np.zeros((num_states, num_actions))
+
+# Select an action according to the current Q-table and an exploration strategy.
+# This could use an epsilon-greedy strategy, for example.
+def select_action(q_table, state, epsilon):
+    if np.random.uniform(0, 1) < epsilon:
+        # Choose a random action.
+        return action_space()
+    else:
+        # Choose the action with the highest estimated value.
+        return np.argmax(q_table[state])
+
+# Update the Q-table using the formula:
+# Q[current_state, current_action] = (1 - alpha) * Q[current_state, current_action] + alpha * (reward + gamma * max(Q[new_state, a]))
+def update_q_table(q_table, current_state, current_action, new_state, reward, alpha, gamma):
+    q_table[current_state, current_action] = (1 - alpha) * q_table[current_state, current_action] + alpha * (reward + gamma * np.max(q_table[new_state]))
+
+# Set the hyperparameters for the Q-learning algorithm.
+num_states = 10
+num_actions = 10
+num_steps = 1000
+alpha = 0.1
+gamma = 0.9
+epsilon = 0.1
+
+# Initialize the Q-table for the bipedal walker.
+q_table = initialize_q_table(num_states, num_actions)
+
+# Run the Q-learning algorithm for the specified number of steps.
+for _ in range(num_steps):
+    # Select a random initial state.
+    current_state = state_space()
+    
+    # Select an action according to the current Q-table and an exploration strategy.
+    current_action = select_action(q_table, current_state, epsilon)
+    
+    # Observe the environment and get the new state and reward.
+    new_state, reward = environment(current_state, current_action)
+    
+    # Update the Q-table.
+    update_q_table(q_table, current_state, current_action, new_state, reward, alpha, gamma)
+    
+# Print the final Q-table.
+print(q_table)
